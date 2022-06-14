311ad19486ad4b0970c04cc2cc06c33af379282b
spielhoelle <spielhoelle@posteo.net>
2022-06-14 20:17:37 +0800

    chore: add eslint

---
+++ wp-content/plugins/gutenpride/.eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'react/prop-types': 0,
+    'react/jsx-props-no-spreading': 0,
+    'react/react-in-jsx-scope': 0,
+    'import/no-unresolved': 0,
+    'react/jsx-filename-extension': 0,
+  },
+};
