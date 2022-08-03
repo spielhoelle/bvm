a06d94090ee36dee089089c15365a4c384882ed6
Thomas Kuhnert <spielhoelle@posteo.net>
2022-08-03 23:51:36 +0900

    feat: display rounded text

--- wp-content/plugins/gutenpride/blocks/circletext/edit.js
+++ wp-content/plugins/gutenpride/blocks/circletext/edit.js
@@ -6,21 +6,44 @@ import './style.scss';
 
 export default function Edit(props) {
   const {
-    attributes, setAttributes,
+    attributes, setAttributes, isSelected
   } = props;
 
   const onChangeTitle = (newTitle) => {
     setAttributes({ circletext: newTitle });
   };
+  // const isSelected = () => wp.data.select('core/blocks').getBlockType('create-block/tmy-circletext');
 
   return (
     <div {...useBlockProps()}>
-      <RichText
-        tagName="p"
-        onChange={onChangeTitle}
-        value={attributes.circletext}
-        placeholder={__('Title...')}
-      />
+      <p className="circletext-text">
+        {isSelected ? (
+          <RichText
+            tagName="p"
+            onChange={onChangeTitle}
+            value={attributes.circletext}
+            placeholder={__('Title...')}
+          />
+        ) : (
+          <svg viewBox="0 0 100 100" width="100" height="100">
+            <defs>
+              <path
+                id="circle"
+                d="
+        M 50, 50
+        m -37, 0
+        a 37,37 0 1,1 74,0
+        a 37,37 0 1,1 -74,0"
+              />
+            </defs>
+            <text fontSize="17">
+              <textPath xlinkHref="#circle">
+                {attributes.circletext}
+              </textPath>
+            </text>
+          </svg>
+        )}
+      </p>
     </div>
   );
 }
--- wp-content/plugins/gutenpride/blocks/circletext/save.js
+++ wp-content/plugins/gutenpride/blocks/circletext/save.js
@@ -8,7 +8,25 @@ export default function save(props) {
   return (
     <div {...blockProps}>
       {attributes.circletext && (
-        <p className="circletext-text">{attributes.circletext}</p>
+        <p className="circletext-text">
+          <svg viewBox="0 0 100 100" width="100" height="100">
+            <defs>
+              <path
+                id="circle"
+                d="
+            M 50, 50
+            m -37, 0
+            a 37,37 0 1,1 74,0
+            a 37,37 0 1,1 -74,0"
+              />
+            </defs>
+            <text fontSize="17">
+              <textPath xlinkHref="#circle">
+                {attributes.circletext}
+              </textPath>
+            </text>
+          </svg>
+        </p>
       )}
     </div>
   );
