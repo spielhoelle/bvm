f49670597b0513db9cd23eec79d46a8434288fba
Thomas Kuhnert <spielhoelle@posteo.net>
2022-08-17 01:09:37 +0900

    chore: update wp-scripts, improve eslint config

--- wp-content/plugins/gutenpride/.eslintrc.js
+++ wp-content/plugins/gutenpride/.eslintrc.js
@@ -21,13 +21,18 @@ module.exports = {
     'react/prop-types': 0,
     'react/jsx-props-no-spreading': 0,
     'react/react-in-jsx-scope': 0,
-    'import/no-unresolved': 0,
+    'import/no-unresolved': [
+      2,
+      { 'caseSensitive': false },
+    ],
     'react/jsx-filename-extension': 0,
     'import/no-extraneous-dependencies': 0,
     'max-len': 0,
     'jsx-a11y/alt-text': 0,
     'semi': [2, 'never'],
-    'quote-props': ['always'],
-    "indent": ["error", 4]
+    'quote-props': 0,
+    'quotes': 0,
+    'react/jsx-curly-brace-presence': 0,
+    'indent': 2,
   },
-};
+}
